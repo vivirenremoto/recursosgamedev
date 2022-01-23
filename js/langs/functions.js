10fea792ca5391880b83a6c2656bb6ae4d34add0
vivirenremoto <gafeman@gmail.com>
2022-01-23 16:35:11 +0100

    i18n

---
+++ js/langs/functions.js
@@ -0,0 +1,46 @@
+
+
+var userLang = navigator.language || navigator.userLanguage; 
+var default_lang = userLang.substr(0,2);
+var exdays = 365;
+
+function changeLang(lang){
+    default_lang = lang;
+    if( i18n[default_lang] ){
+        $('.lang').each(function(){
+            var value = i18n[default_lang][ $(this).data('lang-default') ];
+            $(this).html( value );
+        });
+    }else{
+        $('.lang').each(function(){
+            var value = $(this).data('lang-default');
+            $(this).html( value );
+        });
+    } 
+}
+
+$(function(){
+    $('.dropdown-item').click(function(){
+        default_lang = $(this).attr('href').replace('#','');
+        setCookie('default_lang', default_lang, exdays);
+        changeLang(default_lang);
+    });
+
+    $('.lang').each(function(){
+        $(this).data('lang-default', $(this).html() );
+    });
+
+    if( document.location.href.indexOf('#') > -1 ){
+        default_lang = window.location.hash.replace('#','');
+        setCookie('default_lang', default_lang, exdays);
+        changeLang(default_lang);
+    }
+
+    var cookie_lang = getCookie('default_lang');
+    if( cookie_lang ){
+        default_lang = cookie_lang;
+    }
+
+    changeLang(default_lang);
+})
+
